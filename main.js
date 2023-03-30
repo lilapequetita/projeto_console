3803b098ab3060a78b904ccd8eacb5c0d0606f6e
lilapequetita <114443871+lilapequetita@users.noreply.github.com>
2023-03-30 11:10:19 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+var primeiro_nome="Lila ";
+var ultimo_nome="Camila Chung";
+var familia=primeiro_nome.concat(ultimo_nome);
+console.log(familia);
+
+var num1=12;
+var num2=13;
+var soma=num1+num2;
+console.log("a soma é "+soma);
+var sub=num1-num2;
+console.log("a subtração é "+sub);
+var mult=num1*num2;
+console.log("a multiplicação é"+mult);
+var div=num1/num2;
+console.log("a divisão é "+div);
